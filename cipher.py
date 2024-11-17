7f9bde4d43df954a3a2885a0f05ef36f72982056
bmengel7 <brie.mengel@yahoo.com>
2024-11-16 20:05:42 -0500

    "updated code"

--- cipher.py
+++ cipher.py
@@ -1,1 +1,19 @@
 # add your code here
+message = "python is fun!"
+shift = 5
+result = ""
+
+# go through each of the letters in the message
+# letter to represent input from variable message.
+for letter in message:
+    if letter.isalpha():
+    #convert character
+        letter_code = ord(letter)
+        new_letter_code = shift + letter_code
+    #chr returns the character corresponding to that code point
+        new_character = chr(new_letter_code)
+    #each time loop through, going to append new_character. 
+        result += new_character
+    else:
+        result += letter
+print(result)
